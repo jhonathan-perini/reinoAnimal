a8f9113e7ffe82cd385abcda89b3e36f92bbc5eb
jhonathan-perini <88386345+jhonathan-perini@users.noreply.github.com>
2021-09-19 18:00:11 -0300

    Add files via upload

---
+++ ReinoAnimal_v2/src/Avestruz.java
@@ -0,0 +1,8 @@
+
+public class Avestruz extends Chordata {
+
+	public String classe() {
+		return "Avestruz: \n" + chordata() + "Classe: Aves\n" + "Ordem: Struthioniformes\n" + "Familia: Struthionidae\n"
+				+ "Gênero: Struthio\n";
+	}
+}
---
+++ ReinoAnimal_v2/src/Polimorfismo.java
@@ -0,0 +1,17 @@
+
+public class Polimorfismo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cachorro Cachorro = new Cachorro();
+		Sapo Sapo = new Sapo();
+		Avestruz Avestruz = new Avestruz();
+
+		System.out.println(Cachorro.classe());
+		System.out.println(Sapo.classe());
+		System.out.println(Avestruz.classe());
+
+	}
+
+}
